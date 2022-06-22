34062c3606d0af7859e313b6f342d0333972fcd7
voxxNihili <4valerian@gmail.com>
2022-06-22 14:18:20 +0300

    cart, layout and various other development

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,45 @@
 import Layout from "../components/Layout";
-import { Card, CardContent, CardActions, Button, CardActionArea, CardMedia, Grid, Typography } from "@material-ui/core";
-import data from "../utils/data";
-import NextLink from "next/link"
+import {
+  Card,
+  CardContent,
+  CardActions,
+  Button,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import NextLink from "next/link";
+import db from "../utils/db";
+import Product from "../models/Product";
 
-export default function Home() {
+export default function Home(props) {
+  const { products } = props;
   return (
     <Layout>
       <div>
         <h1>Products</h1>
         <Grid container spacing={3}>
-          {data.products.map((product) => (
+          {products.map((product) => (
             <Grid item md={4} key={product.name}>
               <Card>
                 <NextLink href={`/product/${product.slug}`} passHref>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    image={product.image}
-                    title={product.name}
-                  ></CardMedia>
-                  <CardContent>
-                    <Typography>{product.name}</Typography>
-                  </CardContent>
-                </CardActionArea>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      image={product.image}
+                      title={product.name}
+                    ></CardMedia>
+                    <CardContent>
+                      <Typography>{product.name}</Typography>
+                    </CardContent>
+                  </CardActionArea>
                 </NextLink>
                 <CardActions>
                   <Typography>${product.price}</Typography>
                   <Button size="small" color="primary">
                     Add to Cart
-                  </Button> 
+                  </Button>
                 </CardActions>
               </Card>
             </Grid>
@@ -37,4 +48,15 @@ export default function Home() {
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  await db.connect();
+  const products = await Product.find({}).lean();
+  await db.disconnect();
+  return {
+    props: {
+      products: products.map(db.convertDocToObj),
+    },
+  };
 }
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,6 +1,6 @@
-import React from "react";
-import { useRouter } from "next/router";
-import data from "../../utils/data";
+import React, { useContext } from "react";
+// import { useRouter } from "next/router";
+// import data from "../../utils/data";
 import useStyles from "../../utils/styles";
 import Layout from "../../components/Layout";
 import NextLink from "next/link";
@@ -14,13 +14,29 @@ import {
   Typography,
   Button,
 } from "@material-ui/core";
+import Product from "../../models/Product";
+import db from "../../utils/db";
+import axios from "axios";
+import { Store } from '../../utils/Store';
+import { useRouter } from "next/router";
 
-function ProductScreen() {
-  const classes = useStyles();
+function ProductScreen(props) {
   const router = useRouter();
-  const { slug } = router.query;
-  const product = data.products.find((a) => a.slug === slug);
-
+  const { dispatch } = useContext(Store);
+  const { product } = props;
+  const classes = useStyles();
+  // const router = useRouter();
+  // const { slug } = router.query;
+  // const product = data.products.find((a) => a.slug === slug);
+  const addToCartHandler = async () => {
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock <= 0) {
+      window.alert("Sorry. Product is out of stock.");
+      return;
+    }
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity: 1 } });
+    router.push("/cart");
+  };
   return product ? (
     <Layout title={product.name} description={product.description}>
       <div className={classes.section}>
@@ -114,6 +130,7 @@ function ProductScreen() {
                   type="button"
                   variant="contained"
                   color="primary"
+                  onClick={addToCartHandler}
                   fullWidth
                 >
                   Add to Cart
@@ -132,3 +149,17 @@ function ProductScreen() {
 }
 
 export default ProductScreen;
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  await db.connect();
+  const product = await Product.findOne({ slug }).lean();
+  await db.disconnect();
+  return {
+    props: {
+      product: db.convertDocToObj(product),
+    },
+  };
+}
